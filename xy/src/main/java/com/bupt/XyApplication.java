053e90cc07f9f99e4dd477fd54076ee0847d37c6
Stadpole <2209495405@qq.com>
2018-11-06 21:30:07 +0800

    third

--- xy/src/main/java/com/bupt/XyApplication.java
+++ xy/src/main/java/com/bupt/XyApplication.java
@@ -2,10 +2,12 @@ package com.bupt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class XyApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(XyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(XyApplication.class, args);
+    }
 }
